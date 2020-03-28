15e64f8374095eed6add2f21d317518c77a2aada
Pedro N. Sidou <pnsidou@gmail.com>
2020-03-28 16:48:49 -0300

    feat: sign in page input validation with yup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/fastfeet-logo.png';
 import { Background } from './styles';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required(),
+  password: Yup.string()
+    .min(6)
+    .required(),
+});
 
 function SignIn() {
   const dispatch = useDispatch();

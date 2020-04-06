164635f06c4f7afc3ee8d9e94156620525a38c48
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-06 09:17:39 -0300

    feat: react-redux redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import GlobalStyle from '~/styles/global';
 import '~/config/ReactotronConfig';
+import { store, persistor } from '~/store';
 
 import history from '~/services/history';
 import Routes from './routes';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-      <ToastContainer autoClose={3000} />
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 

ed232fd97deac7c8a4908df382bd967a572fdbff
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-06 11:25:27 -0300

    feat: sign in page input handling
    squash: sign in page input handling

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/fastfeet-logo.png';
-
 import { Background } from './styles';
 
 function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
   return (
     <Background>
-      <form>
-        <img src={logo} />
-        <h>EMAIL</h>
-        <input type="email" placeholder="example@email.com" />
-        <h>PASSWORD</h>
-        <input type="password" placeholder="Type your password" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <img src={logo} alt="" />
+        <h1>EMAIL</h1>
+        <Input name="email" type="email" placeholder="example@email.com" />
+        <h1>PASSWORD</h1>
+        <Input
+          name="password"
+          type="password"
+          placeholder="Type your password"
+        />
         <button type="submit">Sign In</button>
-      </form>
+      </Form>
     </Background>
   );
 }

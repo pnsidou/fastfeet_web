9e5903c1644bfa314184cf19833e871ce44153df
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-06 18:41:47 -0300

    feat: add rendered avatar to deliverymen state

--- src/store/modules/deliverymen/reducer.js
+++ src/store/modules/deliverymen/reducer.js
@@ -1,4 +1,10 @@
+import React from 'react';
+
 import produce from 'immer';
+
+import Avatar from '~/components/Avatar';
+
+const avatarColors = ['#dd8', '#bbe', '#fcc', '#ada'];
 
 const INITIAL_STATE = { list: [] };
 
@@ -6,7 +12,21 @@ export default function deliverymen(state = INITIAL_STATE, action) {
   return produce(state, draft => {
     switch (action.type) {
       case '@deliverymen/LIST_SUCCESS': {
-        draft.list = action.payload;
+        const deliverymen = action.payload.map(deliveryman => {
+          const color =
+            avatarColors[Math.floor(Math.random() * avatarColors.length)];
+
+          const avatar = (
+            <Avatar
+              name={deliveryman.name}
+              url={deliveryman.avatar}
+              color={color}
+            />
+          );
+
+          return { ...deliveryman, avatar };
+        });
+        draft.list = deliverymen;
         break;
       }
       default:

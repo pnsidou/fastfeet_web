fb126d3b6ed9b2a026269d36062092781be17a83
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 08:04:31 -0300

    feat: UpdateDeliveryman page

--- src/pages/UpdateDeliveryman/index.js
+++ src/pages/UpdateDeliveryman/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateDeliveryman } from '~/store/modules/deliverymen/actions';
+
+import FormFrame from '~/components/FormFrame';
+import { deliverymanFormBody, schema } from '~/components/DeliverymanFormBody';
 
 function UpdateDeliveryman() {
-  return <div />;
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const deliveryman = useSelector(state =>
+    state.deliverymen.list.find(deliveryman => deliveryman.id == id)
+  );
+
+  const handleRegister = data => {
+    dispatch(updateDeliveryman(id, data));
+  };
+
+  return (
+    <FormFrame
+      title="Edit deliveryman"
+      schema={schema}
+      handleRegister={handleRegister}
+      formBody={deliverymanFormBody}
+      defaultValue={deliveryman}
+    ></FormFrame>
+  );
 }
 
 export default UpdateDeliveryman;
---
+++ src/pages/UpdateDeliveryman/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Frame = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 40px auto;
+`;

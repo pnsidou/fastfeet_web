abd5cdc4cf004f9573770c0dbec13a8dd05e0245
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 07:57:15 -0300

    feat: update recipient page

--- src/pages/UpdateRecipient/index.js
+++ src/pages/UpdateRecipient/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateRecipient } from '~/store/modules/recipients/actions';
+
+import FormFrame from '~/components/FormFrame';
+import { recipientFormBody, schema } from '~/components/RecipientFormBody';
 
 function UpdateRecipient() {
-  return <div />;
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const recipient = useSelector(state =>
+    state.recipients.list.find(recipient => recipient.id == id)
+  );
+
+  const handleRegister = data => {
+    const number = data.number && Number(data.number);
+    dispatch(updateRecipient(id, { ...data, number }));
+  };
+
+  return (
+    <FormFrame
+      title="Edit recipient"
+      schema={schema}
+      handleRegister={handleRegister}
+      formBody={recipientFormBody}
+      defaultValue={recipient}
+    ></FormFrame>
+  );
 }
 
 export default UpdateRecipient;

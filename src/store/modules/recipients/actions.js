bfc92586f30faff77034c2cddd7bc466a43f7584
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 07:51:41 -0300

    sq: recipients actions/sagas

--- src/store/modules/recipients/actions.js
+++ src/store/modules/recipients/actions.js
@@ -17,3 +17,60 @@ export function listRecipientsFailure() {
     type: '@recipients/LIST_FAILURE',
   };
 }
+
+export function registerRecipient(data) {
+  return {
+    type: '@recipients/REGISTER_REQUEST',
+    payload: data,
+  };
+}
+
+export function registerRecipientSuccess() {
+  return {
+    type: '@recipients/REGISTER_SUCCESS',
+  };
+}
+
+export function registerRecipientFailure() {
+  return {
+    type: '@recipients/REGISTER_FAILURE',
+  };
+}
+
+export function deleteRecipient(id) {
+  return {
+    type: '@recipients/DELETE_REQUEST',
+    payload: { id },
+  };
+}
+
+export function deleteRecipientSuccess() {
+  return {
+    type: '@recipients/DELETE_SUCCESS',
+  };
+}
+
+export function deleteRecipientFailure() {
+  return {
+    type: '@recipients/DELETE_FAILURE',
+  };
+}
+
+export function updateRecipient(id, data) {
+  return {
+    type: '@recipients/UPDATE_REQUEST',
+    payload: { id, data },
+  };
+}
+
+export function updateRecipientSuccess() {
+  return {
+    type: '@recipients/UPDATE_SUCCESS',
+  };
+}
+
+export function updateRecipientFailure() {
+  return {
+    type: '@recipients/UPDATE_FAILURE',
+  };
+}
--- src/store/modules/recipients/sagas.js
+++ src/store/modules/recipients/sagas.js
@@ -1,20 +1,68 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-import { listRecipientsSuccess, listRecipientsFailure } from './actions';
+import {
+  listRecipientsSuccess,
+  listRecipientsFailure,
+  deleteRecipientSuccess,
+  deleteRecipientFailure,
+  registerRecipientSuccess,
+  registerRecipientFailure,
+  updateRecipientSuccess,
+  updateRecipientFailure,
+} from './actions';
 
 import api from '~/services/api';
 
 export function* listRecipients({ payload }) {
   try {
     const response = yield call(api.get, '/recipients');
-
     yield put(listRecipientsSuccess(response.data));
   } catch (err) {
     yield put(listRecipientsFailure());
   }
 }
 
+export function* registerRecipient({ payload }) {
+  try {
+    const response = yield call(api.post, '/recipients', payload);
+
+    yield put(registerRecipientSuccess());
+  } catch (err) {
+    yield put(registerRecipientFailure());
+  }
+}
+
+export function* updateRecipient({ payload }) {
+  try {
+    const { id, data } = payload;
+    const response = yield call(api.put, `/recipients/${id}`, data);
+    console.tron.log('update saga recipient', response);
+    yield put(updateRecipientSuccess());
+  } catch (err) {
+    yield put(updateRecipientFailure());
+  }
+}
+
+export function* deleteRecipient({ payload }) {
+  try {
+    const { id } = payload;
+
+    const response = yield call(api.delete, `/recipients/${id}`);
+
+    yield put(deleteRecipientSuccess());
+  } catch (err) {
+    toast('Failed to delete recipient');
+    yield put(deleteRecipientFailure());
+  }
+}
+
 export default all([
   takeLatest('@recipients/LIST_REQUEST', listRecipients),
-  takeLatest('@deliveries/LIST_REQUEST', listRecipients),
+  takeLatest('@recipients/DELETE_REQUEST', deleteRecipient),
+  takeLatest('@recipients/REGISTER_REQUEST', registerRecipient),
+  takeLatest('@recipients/UPDATE_REQUEST', updateRecipient),
+  takeLatest('@recipients/DELETE_SUCCESS', listRecipients),
+  takeLatest('@recipients/REGISTER_SUCCESS', listRecipients),
+  takeLatest('@recipients/UPDATE_SUCCESS', listRecipients),
 ]);

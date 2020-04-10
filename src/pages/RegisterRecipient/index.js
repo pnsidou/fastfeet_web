3c70ac1279cf7bdf79497448a3be3109d572661b
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 07:56:41 -0300

    feat: RegisterRecipient page

--- src/pages/RegisterRecipient/index.js
+++ src/pages/RegisterRecipient/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { registerRecipient } from '~/store/modules/recipients/actions';
+
+import FormFrame from '~/components/FormFrame';
+import { recipientFormBody, schema } from '~/components/RecipientFormBody';
 
 function RegisterRecipient() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  const handleRegister = data => {
+    const number = data.number && Number(data.number);
+    dispatch(registerRecipient({ ...data, number }));
+  };
+
+  return (
+    <FormFrame
+      title="Register recipient"
+      schema={schema}
+      handleRegister={handleRegister}
+      formBody={recipientFormBody}
+    ></FormFrame>
+  );
 }
 
 export default RegisterRecipient;

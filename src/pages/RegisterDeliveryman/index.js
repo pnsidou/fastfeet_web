65d003df325d65118ca94efeb55af3fd4c6271f7
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 08:03:50 -0300

    feat: RegisterDeliveryman page

--- src/pages/RegisterDeliveryman/index.js
+++ src/pages/RegisterDeliveryman/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { registerDeliveryman } from '~/store/modules/deliverymen/actions';
+
+import FormFrame from '~/components/FormFrame';
+import { deliverymanFormBody, schema } from '~/components/DeliverymanFormBody';
 
 function RegisterDeliveryman() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  const handleRegister = data => {
+    dispatch(registerDeliveryman(data));
+  };
+
+  return (
+    <FormFrame
+      title="Register deliveryman"
+      schema={schema}
+      handleRegister={handleRegister}
+      formBody={deliverymanFormBody}
+    ></FormFrame>
+  );
 }
 
 export default RegisterDeliveryman;

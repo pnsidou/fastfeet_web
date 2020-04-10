76794919ff5812593034047381b44f693a6fa0fd
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 08:07:02 -0300

    feat: RegisterDelivery page

--- src/pages/RegisterDelivery/index.js
+++ src/pages/RegisterDelivery/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { registerDelivery } from '~/store/modules/deliveries/actions';
+
+import FormFrame from '~/components/FormFrame';
+import { deliveryFormBody, schema } from '~/components/DeliveryFormBody';
 
 function RegisterDelivery() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  const handleRegister = data => {
+    console.tron.log(data);
+    dispatch(registerDelivery(data));
+  };
+
+  return (
+    <FormFrame
+      title="Register delivery"
+      schema={schema}
+      handleRegister={handleRegister}
+      formBody={deliveryFormBody}
+    ></FormFrame>
+  );
 }
 
 export default RegisterDelivery;

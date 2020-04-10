974902348c13bb24588ae05448b1cb90eca391d6
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 07:51:18 -0300

    sq: deliveries sagas

--- src/store/modules/deliveries/sagas.js
+++ src/store/modules/deliveries/sagas.js
@@ -1,12 +1,19 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import {
   listDeliveriesSuccess,
   listDeliveriesFailure,
   deleteDeliverySuccess,
+  deleteDeliveryFailure,
+  registerDeliverySuccess,
+  registerDeliveryFailure,
+  updateDeliverySuccess,
+  updateDeliveryFailure,
 } from './actions';
 
 import api from '~/services/api';
+import { deleteDeliverymenFailure } from '../deliverymen/actions';
 
 export function* listDeliveries({ payload }) {
   try {
@@ -17,17 +24,45 @@ export function* listDeliveries({ payload }) {
   }
 }
 
+export function* registerDelivery({ payload }) {
+  try {
+    const response = yield call(api.post, '/deliveries', payload);
+
+    yield put(registerDeliverySuccess());
+  } catch (err) {
+    yield put(registerDeliveryFailure());
+  }
+}
+
+export function* updateDelivery({ payload }) {
+  try {
+    const { id, data } = payload;
+    const response = yield call(api.put, `/deliveries/${id}`, data);
+    yield put(updateDeliverySuccess());
+  } catch (err) {
+    yield put(updateDeliveryFailure());
+  }
+}
+
 export function* deleteDelivery({ payload }) {
   try {
-    const id = payload;
+    const { id } = payload;
+
+    const response = yield call(api.delete, `/deliveries/${id}`);
+
     yield put(deleteDeliverySuccess());
   } catch (err) {
-    yield put(listDeliveriesFailure());
+    toast('Failed to delete delivery');
+    yield put(deleteDeliveryFailure());
   }
 }
 
 export default all([
   takeLatest('@deliveries/LIST_REQUEST', listDeliveries),
   takeLatest('@deliveries/DELETE_REQUEST', deleteDelivery),
+  takeLatest('@deliveries/REGISTER_REQUEST', registerDelivery),
+  takeLatest('@deliveries/UPDATE_REQUEST', updateDelivery),
   takeLatest('@deliveries/DELETE_SUCCESS', listDeliveries),
+  takeLatest('@deliveries/REGISTER_SUCCESS', listDeliveries),
+  takeLatest('@deliveries/UPDATE_SUCCESS', listDeliveries),
 ]);

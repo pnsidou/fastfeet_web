9b55ddd72b9cad61cbf2d3408bc0387d4605447d
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-10 07:52:14 -0300

    sq: root reducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import auth from './auth/reducer';
 import user from './user/reducer';
 import deliverymen from './deliverymen/reducer';
-import recipients from './deliverymen/reducer';
+import recipients from './recipients/reducer';
 import deliveries from './deliveries/reducer';
 import problems from './problems/reducer';
 
